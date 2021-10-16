7d3715e560080f9bd2dc06cbe688b32f46bf7a2d
Majkrzak Piotr <piotr.majkrzak@microsoft.wsei.edu.pl>
2021-10-16 15:21:28 +0200

    Added Book MVC

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibApp_WSEI.Models;
+
+namespace LibApp_WSEI.Controllers
+{
+    public class BooksController : Controller
+    {
+        //GET /Books/Random
+        public IActionResult Random()
+        {
+            var firstBook = new Book() { Name = "English dictionary" };
+
+            return View(firstBook);
+        }
+    }
+}
