f75a2e3f86af58981f14f3fac9f62161407a153a
Majkrzak Piotr <piotr.majkrzak@microsoft.wsei.edu.pl>
2021-10-16 14:58:51 +0200

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LibApp_WSEI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
